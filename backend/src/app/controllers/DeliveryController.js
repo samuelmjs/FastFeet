c77272ed551b9aaeacd0a8990a83e7b9693a5bb3
Samuel Monteiro <samu.mjs@gmail.com>
2020-04-13 01:38:13 -0300

    chore: file cancellation delivery

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -5,7 +5,6 @@ import File from '../models/File';
 import Recipient from '../models/Recipient';
 
 import CreationMail from '../jobs/CreationMail';
-import CancelationMail from '../jobs/CancellationMail';
 import Queue from '../../lib/Queue';
 
 class DeliveryController {
@@ -146,57 +145,6 @@ class DeliveryController {
     }
 
     await delivery.update(req.body);
-
-    return res.json(delivery);
-  }
-
-  async delete(req, res) {
-    const delivery = await Delivery.findByPk(req.params.id, {
-      attributes: [
-        'id',
-        'deliveryman_id',
-        'recipient_id',
-        'canceled_at',
-        'product'
-      ],
-      include: [
-        {
-          model: User,
-          as: 'deliveryman',
-          attributes: ['id', 'name', 'email']
-        },
-        {
-          model: Recipient,
-          as: 'recipient',
-          attributes: ['id', 'name']
-        }
-      ]
-    });
-
-    if (!delivery) {
-      return res.json({ error: 'Delivery not exists' });
-    }
-
-    if (delivery.canceled_at) {
-      return res.json({ error: 'Delivery has already been canceled' });
-    }
-
-    if (delivery.start_date || delivery.end_date) {
-      return res.json({ error: "Delivery can't be canceled" });
-    }
-
-    delivery.canceled_at = new Date();
-
-    await delivery.save();
-
-    await Queue.add(CancelationMail.key, {
-      deliveryman: delivery.deliveryman,
-      product: {
-        id: delivery.id,
-        name: delivery.product
-      },
-      recipient: delivery.recipient.name
-    });
 
     return res.json(delivery);
   }
